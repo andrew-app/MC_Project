ef83ce03fe78b73c16458f80e931fa16ae531fd5
Andrew Appuhamy <andrew.appuhamy@gmail.com>
2022-05-24 11:14:42 +1000

    add qr scanner template

---
+++ qrscanner/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qrscanner',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
